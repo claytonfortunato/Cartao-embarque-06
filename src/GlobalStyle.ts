b3619ca82252fe6e776fd29e2375f4093a5fa847
claytonfortunato <claytonreis10@gmail.com>
2023-03-30 00:53:22 -0300

    feat: styles global do projeto

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(167.96deg, #8257E5 0%, #271A45 100%);
+    height: 100vh
+    }
+`;
