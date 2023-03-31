e9504222d2d05e6438d18e357e3321472ca31100
claytonfortunato <claytonreis10@gmail.com>
2023-03-30 23:27:00 -0300

    feat: components add

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as C from "./styles";
+
+const Home = () => {
+  return (
+    <C.Container>
+      <C.Title>Cartão de embarque</C.Title>
+    </C.Container>
+  );
+};
+
+export default Home;
---
+++ src/components/Home/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 327px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const Title = styled.h3`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
+  color: #ffffff;
+`;
