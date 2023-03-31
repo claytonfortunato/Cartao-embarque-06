b1ad31e0ebfedc68b0b7c01aa24d25113a134915
claytonfortunato <claytonreis10@gmail.com>
2023-03-31 14:14:24 -0300

    feat: add font and component

--- src/components/Ticket/index.tsx
+++ src/components/Ticket/index.tsx
@@ -4,12 +4,16 @@ export const Ticket = () => {
   return (
     <C.Container>
       <C.Content dashed={false}>
-        <C.Info>
+        <C.ContainerInfo>
           <C.Info>
             <C.Label>Voo</C.Label>
             <C.Value>R$955</C.Value>
           </C.Info>
-        </C.Info>
+          <C.Info orientation="flex-end">
+            <C.Label>Data</C.Label>
+            <C.Value>23/02/2023</C.Value>
+          </C.Info>
+        </C.ContainerInfo>
       </C.Content>
     </C.Container>
   );
--- src/components/Ticket/styles.ts
+++ src/components/Ticket/styles.ts
@@ -1,11 +1,57 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+interface Props {
+  dashed?: boolean;
+  orientation?: string;
+}
 
-export const Content = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const Info = styled.div``;
+export const Content = styled.div<Props>`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 24px;
+  border-radius: 24px;
+  background: #fff;
+  padding: 24px 32px;
+  border-top: ${(props) =>
+    props.dashed ? "1px dashed rgba(0,0,0,0.32);" : "none"};
+  border-bottom: ${(props) =>
+    props.dashed ? "1px dashed rgba(0,0,0,0.32);" : "none"};
+`;
 
-export const Label = styled.label``;
+export const ContainerInfo = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-export const Value = styled.p``;
+export const Info = styled.div<Props>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) =>
+    props.orientation != "" ? props.orientation : "flex-start"};
+`;
+
+export const Label = styled.label`
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: -0.02em;
+  color: #000000;
+  opacity: 0.6;
+`;
+
+export const Value = styled.p`
+  line-height: 19px;
+  letter-spacing: -0.02em;
+  color: rgba(0, 0, 0, 0.88);
+`;
